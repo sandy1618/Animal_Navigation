a0efaae72c091040b4f5a139d09348d006ced7ac
sandy1618 <sandeepnayak1.618@gmail.com>
2020-11-22 21:59:37 +0900

    audio control modified

--- animal_motion_control/src/move_animal/scripts/audio_control_joy.py
+++ animal_motion_control/src/move_animal/scripts/audio_control_joy.py
@@ -5,13 +5,13 @@ from sensor_msgs.msg import Joy
 
 def callback(msg):
     if msg.buttons[0]:
-        subprocess.call("aplay ~/Animal_Navigation/animal_motion_control/src/move_animal/scripts/left.wav", shell=True)
+        subprocess.call("aplay -D default:CARD=Device ~/Animal_Navigation/animal_motion_control/src/move_animal/scripts/left.wav", shell=True)
         
     if msg.buttons[1]: 
-        subprocess.call("aplay ~/Animal_Navigation/animal_motion_control/src/move_animal/scripts/front.wav", shell=True)
+        subprocess.call("aplay -D default:CARD=Device ~/Animal_Navigation/animal_motion_control/src/move_animal/scripts/front.wav", shell=True)
     
     if msg.buttons[3]: 	
-        subprocess.call("aplay ~/Animal_Navigation/animal_motion_control/src/move_animal/scripts/right.wav", shell=True)
+        subprocess.call("aplay -D default:CARD=Device ~/Animal_Navigation/animal_motion_control/src/move_animal/scripts/right.wav", shell=True)
 
 def listener():
     rospy.init_node('audio_subscriber')
